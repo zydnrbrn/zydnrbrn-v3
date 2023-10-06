62cd913d4511dee963dbcc1937fed8ce84e58136
zydnrbrn <zidanreborn@gmail.com>
2023-10-06 22:28:09 +0700

    adjust: transform transition on change view mode

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,29 +6,36 @@ import {
 import { Link } from "@nextui-org/link";
 import { HomeLogo, ProjectLogo, EducationLogo } from "./icons";
 import { ThemeSwitch } from "@/components/theme-switch";
+import { Tooltip } from "@nextui-org/tooltip";
 
 export const Navbar = () => {
   return (
     <NextUINavbar
-      className="dark:bg-graymac/50 bg-graymac/30 flex rounded-[80px] my-5"
+      className="dark:bg-graymac/50 bg-graymac/30 flex rounded-[80px] my-5 transform transition ease-in-out duration-200"
       maxWidth="full"
       position="sticky"
     >
       <NavbarContent className="flex items-center w-full px-4" justify="center">
         <NavbarItem>
-          <Link href="/">
-            <HomeLogo className="dark:text-white text-black" />
-          </Link>
+          <Tooltip content="Home 🏠">
+            <Link href="/">
+              <HomeLogo className="transform transition ease-in-out duration-200 dark:text-white text-black" />
+            </Link>
+          </Tooltip>
         </NavbarItem>
         <NavbarItem>
-          <Link href="/project">
-            <ProjectLogo className="dark:text-white text-black" />
-          </Link>
+          <Tooltip content="Project 👷">
+            <Link href="/project">
+              <ProjectLogo className="transform transition ease-in-out duration-200 dark:text-white text-black" />
+            </Link>
+          </Tooltip>
         </NavbarItem>
         <NavbarItem>
-          <Link href="/education">
-            <EducationLogo className="dark:text-white text-black" />
-          </Link>
+          <Tooltip content="Education 📚">
+            <Link href="/education">
+              <EducationLogo className="transform transition ease-in-out duration-200 dark:text-white text-black" />
+            </Link>
+          </Tooltip>
         </NavbarItem>
         <NavbarItem>
           <ThemeSwitch />
