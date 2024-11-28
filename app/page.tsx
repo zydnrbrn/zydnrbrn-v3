db0cdfa8bd3fc3821e7ab6cf817e5f72aa3c00b9
zydnrbrn <zidanreborn@gmail.com>
2024-11-28 12:30:13 +0700

    chore: update hero intro

--- app/page.tsx
+++ app/page.tsx
@@ -10,23 +10,38 @@ export default function Home() {
     <>
       <div className="flex flex-col items-center pt-[200px] pb-[300px]">
         <div className="ring-inset ring-black dark:ring-graymac dark:bg-graymac/50 bg-graymac ring-4 rounded-full w-[300px] h-[300px] flex justify-center items-center">
-          <Image loading="eager" className="w-[200px] h-[250px]" src={imageConfig.smile} alt="Haloooo" />
+          <Image
+            loading="eager"
+            className="w-[200px] h-[250px]"
+            src={imageConfig.smile}
+            alt="Haloooo"
+          />
         </div>
         <div className="welcome-greet py-5 text-center">
-          <h1 className={'text-current pb-4 font-bold text-[40px]'}>Zidan Khulul Sajid</h1>
-          <div className="text-lg font-extralight">
-            <p>Is a <b>Software Engineer</b>, <b>Backend Engineer</b> and <b>React Developer.</b></p>
+          <h1 className={"text-current pb-4 font-bold text-[40px]"}>
+            Zidan Khulul Sajid
+          </h1>
+          <div className="text-lg font-light">
+            <p>
+              Is a <b>Self-Taugh Software Engineer</b>, Familiar with
+              <b>Typescript, Rust and PHP</b>. <br /> Also Familiar with the{" "}
+              <b>Laravel and Next.js Framework</b>.
+            </p>
           </div>
         </div>
         <Socials />
       </div>
       <div className="skilset flex flex-col rounded-[20px] my-[95px] w-full">
-        <h1 className="text-current pb-4 font-bold text-[30px] flex-grow">My Projects</h1>
+        <h1 className="text-current pb-4 font-bold text-[30px] flex-grow">
+          My Projects
+        </h1>
         <Divider className="text-black mb-5" />
         <ListProjects />
       </div>
       <div className="skilset flex flex-col rounded-[20px] my-[95px] w-full">
-        <h1 className="text-current pb-4 font-bold text-[30px] flex-grow">Recent Post</h1>
+        <h1 className="text-current pb-4 font-bold text-[30px] flex-grow">
+          Recent Post
+        </h1>
         <Divider className="text-black mb-5" />
         <ListPosts />
       </div>
